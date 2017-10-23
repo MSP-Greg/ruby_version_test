e42dc07f83543113ce55f714c2a57dc7be091ac6
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-10-23 09:56:31 -0500

    Gem cleanup & list

--- vers_test.cmd
+++ vers_test.cmd
@@ -1,22 +1,41 @@
 @set base_path=C:/Program Files/7-Zip;C:/Program Files/AppVeyor/BuildAgent;C:/Program Files/Git/cmd;C:/Program Files (x86)/GNU/GnuPG/pub;C:/Windows/system32;C:/Windows
 @echo.
-@PATH=C:/Ruby193/bin;%base_path%      & ruby -v & %vers_cmd%
+@PATH=C:/Ruby193/bin;%base_path%      & ruby -v
+@call gem cleanup
+@call gem list
 @echo.
 
-@PATH=C:/ruby200-x64/bin;%base_path%  & ruby -v & %vers_cmd%
+@PATH=C:/ruby200-x64/bin;%base_path%  & ruby -v
+@call gem cleanup -V
+@call gem list
+
 @echo.
 
-@PATH=C:/ruby21-x64/bin;%base_path%   & ruby -v & %vers_cmd%
+@PATH=C:/ruby21-x64/bin;%base_path%   & ruby -v
+@call gem cleanup bundler json openssl rubygems-update -V
+@call gem list
+
 @echo.
 
-@PATH=C:/ruby22-x64/bin;%base_path%   & ruby -v & %vers_cmd%
+@PATH=C:/ruby22-x64/bin;%base_path%   & ruby -v
+@call gem cleanup -V
+@call gem list
+
 @echo.
 
-@PATH=C:/ruby23-x64/bin;%base_path%   & ruby -v & %vers_cmd%
+@PATH=C:/ruby23-x64/bin;%base_path%   & ruby -v
+@call gem cleanup -V
+@call gem list
+
 @echo.
 
-@PATH=C:/ruby24-x64/bin;%base_path%   & ruby -v & %vers_cmd%
+@PATH=C:/ruby24-x64/bin;%base_path%   & ruby -v
+@call gem cleanup -V
+@call gem list
+
 @echo.
 
-@PATH=C:/ruby_trunk/bin;%base_path%   & ruby -v & %vers_cmd%
+@PATH=C:/ruby_trunk/bin;%base_path%   & ruby -v
+@call gem cleanup -V
+@call gem list
 @echo.
