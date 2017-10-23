2c020d6a3355f71782fc8997398bd3ecb8626e1f
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-10-22 23:48:37 -0500

    gem list

---
+++ main_run.cmd
@@ -0,0 +1,15 @@
+@set rb_file=is_07968_socket_send_0010.rb
+@echo   10 Characters Send
+@call vers_test.cmd
+
+@set rb_file=is_07968_socket_print_0010.rb
+@echo   10 Characters Print
+@call vers_test.cmd
+
+@set rb_file=is_07968_socket_send_2000.rb
+@echo 2000 Characters Send
+@call vers_test.cmd
+
+@set rb_file=is_07968_socket_print_2000.rb
+@echo 2000 Characters Print
+@call vers_test.cmd
